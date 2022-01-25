517f91a69e44b0524f81f537d9431d22e5d702bd
Natalie604917 <ni604917@gmail.com>
2022-01-25 13:59:07 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+const element =  ingredients.map(option => {
+    const itemRef = document.createElement('li')
+    itemRef.textContent = option
+    itemRef.classList.add('item')
+    return itemRef
+})
+console.log(ingredientsList)
+ingredientsList.append(...element)
