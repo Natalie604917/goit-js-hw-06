330668a665737d81eb6fc38892d6f23bb924d103
Natalie604917 <ni604917@gmail.com>
2022-02-03 18:07:21 +0200

    -

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyColor: document.querySelector('body'),
+  button: document.querySelector('button'),
+ 
+};
+function changeColor(colorValue) {
+  document.refs.bodyColor.style.background = document.getElementById(colorValue).dataset.color;
+}
